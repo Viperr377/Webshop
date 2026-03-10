9b2b19a7d5361388c77a8ac9e2da4fe07c35980d
Maxim Tarasov <40745@ma-web.nl>
2026-03-10 09:59:44 +0100

    Webshop

---
+++ main.js
@@ -0,0 +1,19 @@
+let auto1 = {
+    kleur: "groen",
+    merk: "Mercedes",
+    hoeveelheidwielen: 4,
+    toeteren(){
+        console.log("toet toet");
+    }
+}
+
+let auto2 = {
+    kleur: "zwart",
+    merk: "BMW",
+    hoeveelheidwielen: 4,
+    toeteren(){
+        console.log("biep biep");
+    }
+}
+auto1.toeteren();
+auto2.toeteren();
