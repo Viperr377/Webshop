4fcc1bb97658786c155350d33384efd43e7ccce8
Maxim Tarasov <40745@ma-web.nl>
2026-03-30 09:56:08 +0200

    Webshop

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
-let auto1 = {
-    kleur: "groen",
-    merk: "Mercedes",
-    hoeveelheidwielen: 4,
-    toeteren(){
-        console.log("toet toet");
-    }
-}
+const products = document.querySelectorAll('.product');
 
-let auto2 = {
-    kleur: "zwart",
-    merk: "BMW",
-    hoeveelheidwielen: 4,
-    toeteren(){
-        console.log("biep biep");
-    }
+const buttons = document.querySelectorAll('.filter-btn');
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const category = btn.dataset.category;
+
+    const target = document.getElementById('target-section');
+    target.scrollIntoView({behavior: 'smooth'
+    })
+
+    products.forEach(product => {
+      if (category === 'All' || product.dataset.category === category) {
+        product.style.display = 'block';
+      } else {
+        product.style.display = 'none';
+      }
+    });
+  });
+});
+
+const searchInput = document.getElementById('searchInput');
+
+searchInput.addEventListener('keydown', e => {
+  if (e.key === 'Enter') {
+    const query = searchInput.value.toLowerCase();
+
+    const target = document.getElementById('target-section');
+    target.scrollIntoView({behavior: 'smooth'});
+
+    products.forEach(product => {
+      const title = product.querySelector('h4').textContent.toLowerCase();
+      if (title.includes(query)) {
+        product.style.display = 'block';
+      } else {
+        product.style.display = 'none';
+      }
+    });
 }
-auto1.toeteren();
-auto2.toeteren();
+});
